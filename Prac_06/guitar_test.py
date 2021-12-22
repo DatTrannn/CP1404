e053319d25e94f54f443c5a5eb4a5ca9537e4223
Dattran <dat0609.tran@gmail.com>
2021-12-22 18:39:39 +0700

    Complete guitar_test.py

---
+++ Prac_06/guitar_test.py
@@ -0,0 +1,9 @@
+from guitar import Guitar
+
+gibson_guitar = Guitar("Gibson L-5 CES", 1922, 16035.40)
+another_guitar = Guitar("Another Guitar", 2013)
+
+print(f"{gibson_guitar.name} get_age() - Expected 99. Got {gibson_guitar.get_age()}")
+print(f"{another_guitar.name} get_age() - Expected 8. Got {another_guitar.get_age()}")
+print(f"{gibson_guitar.name} is_vintage() - Expected True. Got {gibson_guitar.is_vintage()}")
+print(f"{another_guitar.name} is_vintage() - Expected False. Got {another_guitar.is_vintage()}")
