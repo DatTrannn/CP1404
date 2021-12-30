63ac0d1aab66bcf84d35af2b64c1583b862704de
Dattran <dat0609.tran@gmail.com>
2021-12-31 01:08:50 +0700

    Refactor box_layout program to create a simple GUI similar to your greeting app that lets a user type in their score out of 100, click a button and the app shows "Pass" or "Fail" (based on the value) in a label.
    Then extend this to show the JCU grade (e.g. 65-75 is a credit).

--- Prac_07/box_layout_demo.py
+++ Prac_07/box_layout_demo.py
@@ -1,8 +1,11 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import StringProperty
 
 
 class BoxLayoutDemo(App):
+    grade = StringProperty()
+
     def build(self):
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
@@ -14,6 +17,22 @@ class BoxLayoutDemo(App):
 
     def clear_all(self):
         self.root.ids.output_label.text = self.root.ids.input_name.text = ''
+
+    def calculate_grade(self):
+        try:
+            score = int(self.root.ids.input_score.text)
+            if score < 50:
+                self.grade = "Fail"
+            elif score < 65:
+                self.grade = "Pass"
+            elif score < 75:
+                self.grade = "Credit"
+            elif score < 85:
+                self.grade = "Distinction"
+            elif score <= 100:
+                self.grade = "High Distinction"
+        except ValueError:
+            self.grade = "Invalid input"
 
 
 BoxLayoutDemo().run()
