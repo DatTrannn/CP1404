a8b17e961c3d0d644f88b7401b7982cb5063b157
Dattran <dat0609.tran@gmail.com>
2022-01-12 18:01:32 +0700

    Reformat get_fixed_filename function

--- Prac_09/cleanup_files.py
+++ Prac_09/cleanup_files.py
@@ -2,54 +2,22 @@
 CP1404/CP5632 Practical
 Demos of various os module examples
 """
-import shutil
-import os
-
-
-def main():
-    """Demo os module functions."""
-    print("Starting directory is: {}".format(os.getcwd()))
-
-    os.chdir('Lyrics/Christmas')
-
-    print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
-
-    try:
-        os.mkdir('temp')
-    except FileExistsError:
-        pass
-
-    for filename in os.listdir('.'):
-        if os.path.isdir(filename):
-            continue
-
-        new_name = get_fixed_filename(filename)
-        print("Renaming {} to {}".format(filename, new_name))
-
-        os.rename(filename, new_name)
-
-        shutil.move(filename, 'temp/' + new_name)
 
 
 def get_fixed_filename(filename):
     """Return a 'fixed' version of filename."""
-    new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
+    filename = filename.replace(" ", "_").replace(".TXT", ".txt")
+    new_name = ""
+    for i, char in enumerate(filename):
+        if char.isupper() and filename[i - 1:i].islower():
+            new_name += f'_{char}'
+        elif filename[i - 1:i] == ' ':
+            new_name += ''
+        elif (filename[i - 1:i] == '_' or filename[i - 1:i] == '(') and char.islower():
+            new_name += char.upper()
+        else:
+            new_name += char
     return new_name
 
 
-def demo_walk():
-    """Process all subdirectories using os.walk()."""
-    os.chdir('Lyrics')
-    for directory_name, subdirectories, filenames in os.walk('.'):
-        print("Directory:", directory_name)
-        print("\tcontains subdirectories:", subdirectories)
-        print("\tand files:", filenames)
-        print("(Current working directory is: {})".format(os.getcwd()))
-
-        for filename in filenames:
-            os.rename(os.path.join(directory_name, filename),
-                      os.path.join(directory_name, get_fixed_filename(filename)))
-
-
-# main()
-demo_walk()
+print(get_fixed_filename("ItIsWell (oh my soul).txt"))
